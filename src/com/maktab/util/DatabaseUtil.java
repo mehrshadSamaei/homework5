1819c0507a01db36d0a1e224676dfba7c33d3542
mehrshadsamaei <mehrshadsamaei0074@gmail.com>
2022-05-20 09:00:40 -0700

    connection database

---
+++ src/com/maktab/util/DatabaseUtil.java
@@ -0,0 +1,25 @@
+package com.maktab.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private String url;
+    private String userName;
+    private String password;
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public DatabaseUtil() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.url = "jdbc:mysql://localhost:3306/onlineshop";
+        this.userName = "root";
+        this.password = "Mehrshad@1374";
+
+        this.connection = DriverManager.getConnection(url , userName , password);
+    }
+}
